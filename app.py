bd08025966400cf2ff40bca44f708ca1ce19017a
yattinda <yattinda@gmail.com>
2023-09-13 23:03:13 +0900

    Add CORS

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/', methods=["GET"])
 def test():
@@ -11,7 +13,6 @@ def post_acceleration_data():
     json = request.get_json()
     First = json["first"]
     Second = json["second"]
-    
     return "AAAAAAAAAAAAAAAAA"
 
 if __name__ == '__main__':
