66670e823a6dcda3a7aae0f656950b90141b4c86
yattinda <yattinda@gmail.com>
2023-09-14 04:44:53 +0900

    culc max

--- app.py
+++ app.py
@@ -1,8 +1,32 @@
 from flask import Flask, request
 from flask_cors import CORS
+import json
+import pandas as pd
+from math import sqrt
 
 app = Flask(__name__)
 CORS(app)
+
+Mets = {
+    "stand" : 2,
+    "walk"  : 4,
+    "jump"  : 6,
+}
+
+jump_min = 5
+walk_min = 2
+
+def culc_acceleration(x, y, z):
+    return sqrt(x**2 + y**2 + z**2)
+
+def culc_cal(acceleration, second, weight):
+    if acceleration > jump_min:
+        mets = "jump"
+    elif walk_min > acceleration:
+        mets = "stand"
+    else:
+        mets = "walk"
+    return float(Mets[mets] * (second / 3600) * weight * 1.05)
 
 @app.route('/', methods=["GET"])
 def test():
@@ -10,10 +34,23 @@ def test():
 
 @app.route('/', methods=["POST"])
 def post_acceleration_data():
-    json = request.get_json()
-    First = json["first"]
-    Second = json["second"]
-    return "AAAAAAAAAAAAAAAAA"
+    total_cal = 0.0
+    first_index = 0
+    last_index = 10
+    df_acceration = pd.DataFrame(columns=["acceration"])
+    posted_json = request.get_json()
+    weight = posted_json["weight"]
+    raw_df_acceration = posted_json['DATA']
+    for i in range(len(raw_df_acceration)):
+        acceration = culc_acceleration(raw_df_acceration[i]["x"], raw_df_acceration[i]["y"], raw_df_acceration[i]["z"])
+        df_acceration.loc[i] = acceration
+    while last_index < len(raw_df_acceration):
+        target = df_acceration[first_index:last_index]
+        acceration_max = float(target.max(axis = 0))
+        total_cal += culc_cal(acceration_max, 1, weight)
+        first_index += 10
+        last_index += 10
+    return str(total_cal)
 
 if __name__ == '__main__':
     app.run()
