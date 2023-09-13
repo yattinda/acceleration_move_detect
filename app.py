4b8273cfe852a4613cded8e44cc109ae13339a89
yattinda <yattinda@gmail.com>
2023-09-14 04:50:18 +0900

    Culc each

--- app.py
+++ app.py
@@ -35,21 +35,12 @@ def test():
 @app.route('/', methods=["POST"])
 def post_acceleration_data():
     total_cal = 0.0
-    first_index = 0
-    last_index = 10
-    df_acceration = pd.DataFrame(columns=["acceration"])
     posted_json = request.get_json()
     weight = posted_json["weight"]
     raw_df_acceration = posted_json['DATA']
     for i in range(len(raw_df_acceration)):
         acceration = culc_acceleration(raw_df_acceration[i]["x"], raw_df_acceration[i]["y"], raw_df_acceration[i]["z"])
-        df_acceration.loc[i] = acceration
-    while last_index < len(raw_df_acceration):
-        target = df_acceration[first_index:last_index]
-        acceration_max = float(target.max(axis = 0))
-        total_cal += culc_cal(acceration_max, 1, weight)
-        first_index += 10
-        last_index += 10
+        total_cal += culc_cal(acceration, 0.1, weight)
     return str(total_cal)
 
 if __name__ == '__main__':
